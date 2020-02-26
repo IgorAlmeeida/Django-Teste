8c02f683268d8d4298d60f1e444beef5409b6f8a
ShiroeTwo <igoralmeeida@hotmail.com>
2020-02-26 12:14:56 -0300

    Commit 3

--- meuProjeto1/clientes/models.py
+++ meuProjeto1/clientes/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Cliente (models.Model):
     name = models.CharField(max_length=200, blank= False, null=False)
     endereco = models.CharField(max_length=200, blank= False, null= False)
+    salario = models.DecimalField(max_digits=8,decimal_places=2)
+    idade = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
