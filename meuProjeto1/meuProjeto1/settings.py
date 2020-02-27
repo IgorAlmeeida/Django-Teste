952b72c7bad169e346813304121ec17b878293ed
ShiroeTwo <igoralmeeida@hotmail.com>
2020-02-27 12:10:34 -0300

    Estudando templates

--- meuProjeto1/meuProjeto1/settings.py
+++ meuProjeto1/meuProjeto1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'meuProjeto1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates_site'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
