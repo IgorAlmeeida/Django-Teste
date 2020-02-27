c08b51136dea9187828108216eec2f3ca5d52d8d
ShiroeTwo <igoralmeeida@hotmail.com>
2020-02-26 21:54:54 -0300

    Personalizando a exibição do DjangoAdmin

--- meuProjeto1/clientes/admin.py
+++ meuProjeto1/clientes/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Cliente, Departamento, CPF, Telefone
 # Register your models here.
 
-admin.site.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    fields = ('name','endereco','salario','idade','email','cpf')
+    list_display = ('name', 'cpf', 'email')
+    list_filter = ('departamento',)
+    search_fields = ('name', 'cpf', 'email', 'departamento')
+
+admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Departamento)
 admin.site.register(CPF)
 admin.site.register(Telefone)
